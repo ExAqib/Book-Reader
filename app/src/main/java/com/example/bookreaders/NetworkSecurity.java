01f09c54c8e9586f821d26711fe712acca22bb32
aqibn <aqibnaveed272@gmail.com>
2022-02-20 00:30:05 +0500

    One Signal Push Notification Added

--- app/src/main/java/com/example/bookreaders/NetworkSecurity.java
+++ app/src/main/java/com/example/bookreaders/NetworkSecurity.java
@@ -3,12 +3,23 @@ package com.example.bookreaders;
 import android.app.Application;
 
 import com.google.firebase.database.FirebaseDatabase;
+import com.onesignal.OneSignal;
 
 public class NetworkSecurity extends Application {
+    private static final String ONESIGNAL_APP_ID = "f93ad822-0cb5-4a32-85e3-b94ee9dd82ed";
+
     @Override
     public void onCreate() {
         super.onCreate();
         FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+
+        //One Signal's
+        // Enable verbose OneSignal logging to debug issues if needed.
+        OneSignal.setLogLevel(OneSignal.LOG_LEVEL.VERBOSE, OneSignal.LOG_LEVEL.NONE);
+
+        // OneSignal Initialization
+        OneSignal.initWithContext(this);
+        OneSignal.setAppId(ONESIGNAL_APP_ID);
     }
 }
 
